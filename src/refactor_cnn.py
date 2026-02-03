3d8cbc400dca8ea4c773992d87e83c61a8345ea0
Christian <christianhooper@protonmail.com>
2026-02-03 14:54:29 -0800

    Continued to work on colvolution class setup.

--- src/refactor_cnn.py
+++ src/refactor_cnn.py
@@ -9,12 +9,54 @@ from PIL import Image
 
 class Convolution():
     def __init__(self,
-    layer_number:int,
-    forward_data:np.ndarray,
-    input_shape:int
+        layer_number:int,
+        forward_data:np.ndarray,
+        input_shape:tuple,
+        kernel:int,
+        output_multiple:int,
+        stride:int,
+        padding:int,
+        bias:int
     ):
+        self.layer_number = layer_number
+        self.X = forward_data
+        self.N, self.C_i, self.H, self.H = input_shape
+        self.C_o = self.C_i * output_multiple
+        self.fl = kernel
+        self.s = stride
+        self.p = padding
+        print(np.floor(self.H + (2*self.p) - self.fl)/self.s)
 
-        None
+        # Container for storing data input for calculation $\lfloor{\frac{length+2p}{s}}\rfloor$
+        self.X_c = np.zeros(
+            ( self.N,
+            self.C_o,
+            int((self.H + (2*self.p))/self.s),
+            int((self.H + (2*self.p))/self.s)
+            )
+        )
+        self.b = np.full(self.C_o, bias)
+        self.W = self.set_weights()
+        
+        print(f'Data: {self.X.shape}')
+        print(f'Data Padded: {self.X_c.shape}')
+        print(f'Weights: {self.W.shape}')
+        print(f'Bias:  {self.b.shape}')
+
+
+    def set_weights(self) -> np.ndarray :
+        """
+        Creates Xavier-scaled weights for the convolution kernels.
+
+        Returns
+        -------
+        np.ndarray
+            Tensor shaped (out_channels, in_channels, kernel_h, kernel_w).
+        """
+        fan_in  = self.C_i * self.fl * self.fl # In-channels, kernel_h,
+        fan_out = self.C_o  * self.fl * self.fl # Out-channels, kernel_h, kernel_w
+        limit   = (-np.sqrt(6.0 / (fan_in + fan_out)), np.sqrt(6.0 / (fan_in + fan_out)))
+        return  rng.uniform(*limit, size=(self.C_o, self.C_i, self.fl, self.fl)) #(2, 1, 5, 5)
 
 
 class Activation():
@@ -65,19 +107,24 @@ if __name__ == "__main__":
     print("EVAL\n", evaluation)
 
     data_raw = np.array([[np.array(Image.open(p).convert("L"), dtype=np.float32) / 255.0] for p in total_paths])
-    print("SHAPE: ", data_raw.shape.type)
+    print("SHAPE: ", data_raw.shape)
 
     kernel_size = 5
     output_increase = 2
     convolution_stride = 1
+    padding_thickness = kernel_size//2
+    bias_base = 0.1
 
+    # Creates first layer of network
     cl_0 = Convolution(
         layer_number = 0,
         forward_data = data_raw,
-        input_shape = raw_data.shape,
+        input_shape = data_raw.shape,
         kernel = kernel_size,
         output_multiple = output_increase,
-        stride = convolution_stride
+        stride = convolution_stride,
+        padding = padding_thickness,
+        bias = bias_base
     )
 
 
